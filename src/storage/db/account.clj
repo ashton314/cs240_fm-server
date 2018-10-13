d051f3738fbaeada9b236d46138a00bab463d1d1
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2018-10-13 14:02:46 -0600

    account storage and retrieval seems to work

--- src/storage/db/account.clj
+++ src/storage/db/account.clj
@@ -13,7 +13,7 @@
     AccountStorage
 
     (create! [self]
-      (let [ret (jdbc/insert! db-spec "accounts" {:created (util/sql-now)})]
+      (let [ret (jdbc/insert! db-spec "accounts" {:created_at (util/sql-now)})]
         (-> ret first seq first (get 1))))
 
     (save! [self packed-account]
@@ -24,3 +24,20 @@
 
     (find-username [self username]
       (first (jdbc/query db-spec ["SELECT * FROM accounts WHERE username = ?" username]))))
+
+(defn migrate!
+  "Creates the accounts table if it doesn't exist."
+  [db-store]
+  (jdbc/db-do-commands (:db-spec db-store)
+                       "CREATE TABLE IF NOT EXISTS accounts(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  created_at TEXT DEFAULT(DATETIME('now')),
+  updated_at TEXT DEFAULT(DATETIME('now')),
+  username TEXT,
+  password TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  gender TEXT,
+  root_person INTEGER
+);"))
---
+++ test/fm_server/db_test.clj
@@ -0,0 +1,32 @@
+(ns fm-server.db-test
+  (:require [storage.db.account :as account-storage]
+            [storage.account-protocol :as acc-proto]
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]))
+
+(deftest db-test
+  (let [store (account-storage/map->AccountDbStorage
+               {:db-spec {:dbtype "sqlite" :dbname "/tmp/fm-server-test.db"}})]
+    (testing "creation"
+      (is (account-storage/migrate! store))
+      (is (> (acc-proto/create! store) 0)))))
+
+(deftest crud-test
+  (let [store (account-storage/map->AccountDbStorage
+               {:db-spec {:dbtype "sqlite" :dbname "/tmp/fm-server-test.db"}})]
+    (io/delete-file (:dbname (:db-spec store)))
+    (account-storage/migrate! store)
+    (let [packed-account {:id (acc-proto/create! store)
+                          :first_name "Homestar"
+                          :last_name "Runner"
+                          :username "homestar"
+                          :password "marzipan"
+                          :email "hr@email.com"
+                          :gender "male"
+                          :root_person -1}]
+      (testing "create retrieve update"
+        (prn (acc-proto/save! store packed-account))
+        (is (= packed-account (dissoc (acc-proto/fetch store (:id packed-account))
+                                      :created_at :updated_at)))
+        (is (= packed-account (dissoc (acc-proto/find-username store "homestar")
+                                      :created_at :updated_at)))))))
