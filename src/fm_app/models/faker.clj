37ac59bcb164dfc54e19b9a25a11556bbdc297b1
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2018-10-23 19:08:43 -0600

    faker class uses teacher-supplied fake data

--- src/fm_app/models/faker.clj
+++ src/fm_app/models/faker.clj
@@ -4,3 +4,12 @@
   (:require [clojure.data.json :as json])
   (:gen-class))
 
+(defn gen-name
+  "Generates a name"
+  [gender placement]
+  (let [src-file (if (= placement :first)
+                   (if (= gender :m)
+                     "resources/faker_data/mnames.json"
+                     "resources/faker_data/fnames.json")
+                   "resources/faker_data/snames.json")]
+    (rand-nth (:data (json/read-str (slurp src-file) :key-fn keyword)))))
